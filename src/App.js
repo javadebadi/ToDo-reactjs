69ef5a063b4cac2be2ce68ff10bda81836658919
javadebadi <javad.ebadi.1990@gmail.com>
2020-11-08 01:41:08 +0330

    Update: added some checkboxes to the App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,13 @@ import React from 'react'
 function App () {
   return (
     <div>
-      Let's go
+      <input type="checkbox"/>
+      <label> Read</label><br/>
+      <input type="checkbox"/>
+      <label>Exercise</label><br/>
+      <input type="checkbox"/>
+      <label>Code</label><br/><br/>
+      <input type="submit" value="Submit"/>
     </div>
   )
 }
