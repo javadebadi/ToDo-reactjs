5eb2e49f78cb520a9435c150d9d6f7a9dc54d86f
javadebadi <javad.ebadi.1990@gmail.com>
2020-11-19 00:47:07 +0330

    Update: updated TodoItem to have conditional rendering. When a todo item is done, we give other style to the item, it's color is gray and there is a line through that.

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -3,6 +3,17 @@ import React from 'react'
 
 function TodoItem (props) {
 
+  let styleDone = {
+    color: "rgba(0,0,0,0.3)",
+    textDecoration: "line-through"
+  }
+
+  let styleNotDone = {
+    color: "rgba(30,180,50,1)"
+  }
+
+  let style = props.data.done ? styleDone : styleNotDone
+
   return (
     <div className="todo-item">
       <input type="checkbox"
@@ -10,7 +21,7 @@ function TodoItem (props) {
         onChange={ () => props.onchange(props.data.id)}
         checked={props.data.done}
       />
-      <label className="todo-item-label">
+      <label className="todo-item-label" style={style}>
         {props.data.name}
       </label>
       <br/>
